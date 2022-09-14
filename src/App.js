7cc31c100ec61c81cee11c459c2b654ddab2d081
hackerharsh-77 <r.harsh0177@gmail.com>
2022-09-14 20:45:05 +0530

    created login/register pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+    <Home />
+    </div>
+  );
+}
+
+export default App;
